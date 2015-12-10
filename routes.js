26a95170c266880ced9bedbf403b7bfdfae6523c
Mina <wilhelmina.jane@gmail.com>
2015-12-10 15:45:58 +0000

    attempt to write app in pure node

--- routes.js
+++ routes.js
@@ -1,10 +1,26 @@
-var router = require('router')();
+var https = require('https');
+var url = require('url');
+var router = require('routes')();
+
+
+var busstop = 'https://api.tfl.gov.uk/StopPoint/490012632K?app_id='+ process.env.APP_ID +'&app_key=' + process.env.APP_KEY;
 
 function routes (request, response) {
 
-    response.writeHead(200, {'Content-Type' : 'text/html'});
-    response.write("Hello world"); //response body
-    response.end(); // close connection
+    https.get(busstop, function (response) {
+
+        response.on('data', function () {
+
+            response.write(response);
+        });
+        response.on('err', function (err) {
+
+            console.log(err);
+            response.writeHead(500);
+            response.write("There was an error.");
+            response.end();
+        });
+    });
 }
 
 module.exports = routes;
