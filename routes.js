3c569ab601a7b338db1a9331fbf3df293ffca92c
Mina <wilhelmina.jane@gmail.com>
2015-12-11 16:29:24 +0000

    Get sorted bus arrival times for St Barnabas Church

--- routes.js
+++ routes.js
@@ -1,16 +1,36 @@
 var Wreck = require('wreck');
 
-var busstop = 'https://api.tfl.gov.uk/StopPoint/490012632K?app_id='+ process.env.APP_ID +'&app_key=' + process.env.APP_KEY;
+var stBarnabasChurch = '490012633S';
+var arrivals = 'https://api.tfl.gov.uk/Line/277/Arrivals?stopPointId='+ stBarnabasChurch +'&app_id=' + process.env.APP_ID + '&app_key=' + process.env.APP_KEY;
+
+var options = {
+    payload: 'string'
+};
 
 var routes = [
     {
         method: 'GET',
         path: '/',
-        handler: function (request, reply) {
-            console.log("inside handler");
-            Wreck.get(busstop, function (err, response, payload) {
+        handler: function getBusArrivals(request, reply) {
 
-                reply(payload.toString());
+            Wreck.request('GET', arrivals, options, function (err, res) {
+
+                Wreck.read(res, {json: 'smart'}, function (err, body) {
+
+                    var nextTwoDepartures = body.sort(function (a, b) {
+
+                        if (a.expectedArrival < b.expectedArrival) {
+                            return -1;
+                        } else if (a.expectedArrival > b.expectedArrival) {
+                            return 1;
+                        } else {
+                            return 0;
+                        }
+                    });
+
+                    console.log(nextTwoDepartures);
+                    reply(nextTwoDepartures.toString());
+                });
             });
         }
     }
