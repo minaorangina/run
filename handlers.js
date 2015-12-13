da980f5dcaecdf20f34c36b9a423fe55e1049679
Mina <wilhelmina.jane@gmail.com>
2015-12-13 12:26:21 +0000

    now use request module with request defaults setup

--- handlers.js
+++ handlers.js
@@ -1,45 +1,27 @@
-var Wreck = require('wreck');
-var credentials = '&app_id=' + process.env.APP_ID + '&app_key=' + process.env.APP_KEY;
+var request = require('request');
+
 var stBarnabasChurch = '490012633S';
 var busNumber = '277';
 
+var credentials = '&app_id=' + process.env.APP_ID + '&app_key=' + process.env.APP_KEY;
 var busArrivalsForOneLine = 'Line/'+ busNumber +'/Arrivals?stopPointId='+ stBarnabasChurch;
 var allBusArrivals = 'StopPoint/' + stBarnabasChurch + '/Arrivals';
 
-var options  = {
-
-    baseUrl: 'https://api.tfl.gov.uk'
-};
-
-
+var tfl = request.defaults({
+    baseUrl: 'https://api.tfl.gov.uk',
+    qs: credentials
+});
 
 var handlers = {
 
     getBusArrivals: function (request, reply) {
 
-        var thing = Wreck.request('GET', allBusArrivals + credentials, options, function optionalCallback (err, res) {
-            console.log(res.statusCode);
-            if (err) {
-                return reply(err);
-            } else if (res.statusCode >= 400 && res.statusCode < 500) {
-                return reply("Whoops");
-            }
-            Wreck.read(res, null, function (err, body) {
+        tfl.get(allBusArrivals, function (err, response, body) {
 
-                var nextArrivals = body.sort(function (a, b) {
-
-                    if (a.expectedArrival < b.expectedArrival) {
-                        return -1;
-                    } else if (a.expectedArrival > b.expectedArrival) {
-                        return 1;
-                    } else {
-                        return 0;
-                    }
-                });
-
-                reply(nextArrivals);
-            });
+            reply(body);
         });
+
+
     }
 };
 
