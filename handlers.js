b8c09bb95acdeeb8517b67a13447a1f076409c9e
Mina <wilhelmina.jane@gmail.com>
2015-12-13 01:03:41 +0000

    add baseURL

--- handlers.js
+++ handlers.js
@@ -1,18 +1,28 @@
 var Wreck = require('wreck');
-
+var credentials = '&app_id=' + process.env.APP_ID + '&app_key=' + process.env.APP_KEY;
 var stBarnabasChurch = '490012633S';
 var busNumber = '277';
-var arrivals = 'https://api.tfl.gov.uk/Line/'+ busNumber +'/Arrivals?stopPointId='+ stBarnabasChurch +'&app_id=' + process.env.APP_ID + '&app_key=' + process.env.APP_KEY;
+
+var busArrivalsForOneLine = 'Line/'+ busNumber +'/Arrivals?stopPointId='+ stBarnabasChurch;
+var allBusArrivals = 'StopPoint/' + stBarnabasChurch + '/Arrivals';
+
+var options  = {
+
+    baseUrl: 'https://api.tfl.gov.uk/'
+};
 
 var handlers = {
 
     getBusArrivals: function (request, reply) {
 
-        Wreck.request('GET', arrivals, null, function (err, res) {
-
+        Wreck.request('GET', allBusArrivals + credentials, options, function (err, res) {
+            console.log("res");
+            if (err) {
+                reply(err);
+            }
             Wreck.read(res, {json: 'smart'}, function (err, body) {
 
-                var nextTwoDepartures = body.sort(function (a, b) {
+                var nextArrivals = body.sort(function (a, b) {
 
                     if (a.expectedArrival < b.expectedArrival) {
                         return -1;
@@ -23,9 +33,8 @@ var handlers = {
                     }
                 });
 
-                console.log(nextTwoDepartures);
-                
-                reply(nextTwoDepartures);
+
+                reply(nextArrivals);
             });
         });
     }
