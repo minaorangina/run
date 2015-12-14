cbf9e40a5ab0599f986070f187a3a4e5b3c54597
Mina <wilhelmina.jane@gmail.com>
2015-12-14 13:06:52 +0000

    add app container

---
+++ src/app-container.jsx
@@ -0,0 +1,16 @@
+"use strict";
+var React = require('react');
+
+var AppContainer = React.createClass({
+
+    render: function () {
+
+        return (
+            <div>
+                <h1>Run??</h1>
+            </div>            
+        );
+    }
+});
+
+module.exports = AppContainer;
