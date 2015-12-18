54faf04121b838dd2eb420135919623165913d4f
Mina <wilhelmina.jane@gmail.com>
2015-12-18 18:18:24 +0000

    add request for West Ham DLR arrivals and refactor

--- handlers.js
+++ handlers.js
@@ -1,21 +1,20 @@
 var request = require('request');
 
-var stBarnabasChurch = '490012633S';
-var busNumber = '277';
-
 var credentials = '&app_id=' + process.env.APP_ID + '&app_key=' + process.env.APP_KEY;
-var busArrivalsForOneLine = 'Line/'+ busNumber +'/Arrivals?stopPointId='+ stBarnabasChurch;
-var allBusArrivals = 'StopPoint/' + stBarnabasChurch + '/Arrivals';
 
 var tfl = request.defaults({
     baseUrl: 'https://api.tfl.gov.uk',
     qs: credentials
 });
 
+var woolwichDLR = '940GZZDLWLA';
 
 var handlers = {
 
     getBusArrivals: function (request, reply) {
+
+        var stBarnabasChurch = '490012633S';
+        var allBusArrivals = 'StopPoint/' + stBarnabasChurch + '/Arrivals';
 
         tfl.get(allBusArrivals, function (err, response, body) {
 
@@ -36,8 +35,29 @@ var handlers = {
 
     getDLRArrivals: function (request, reply) {
 
+        var westHamDLR = '940GZZDLWHM';
+        var westHamDLRArrivals = 'StopPoint/' + westHamDLR + '/Arrivals';
 
-        reply (["dlr arrivals!"]);
+        tfl.get(westHamDLRArrivals, function (err, response, body) {
+
+            var results = JSON.parse(body).filter(function (arrival) {
+
+                return arrival.destinationNaptanId === woolwichDLR;
+            })
+            .sort(function (a, b) {
+
+                if (a.expectedArrival < b.expectedArrival) {
+                    return -1;
+                } else if (a.expectedArrival > b.expectedArrival) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            });
+
+            console.log(results.slice(0, 5));
+            // reply(sorted.slice(0, 5));
+        });
     }
 };
 
