0bf1898d576d25744e5f436823d2beb18b482b67
Mina <wilhelmina.jane@gmail.com>
2015-12-26 13:28:41 +0000

    restructuring of source files

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,9 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var AppContainer = require('./src/app-container.jsx');
+var AppContainer = require('./app-container.jsx');
 
 ReactDOM.render(
-    
+
     <AppContainer />,
     document.getElementsByClassName("container")[0]
 );
