3d135443387d507effdb27c564c15473f9d921f5
Mina <wilhelmina.jane@gmail.com>
2016-01-02 13:23:57 +0000

    WIP configuing testing

--- src/test/unit/app-container.js
+++ src/test/unit/app-container.js
@@ -1,1 +1,18 @@
-import mocha from 'mocha';
+"use strict";
+
+import React from 'react';
+import chai from 'chai';
+import TestUtils from 'react-addons-test-utils';
+import { createRenderer } from 'react-addons-test-utils';
+
+const assert = chai.assert;
+
+const testAppContainer = () => {
+
+    assert.ok("1", "1", "Equality found");
+};
+
+export { testAppContainer };
+
+
+// console.log(TestUtils);
