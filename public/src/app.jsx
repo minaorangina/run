df9296581b50e0e9d594ac7a381f5ae3153da869
Mina <wilhelmina.jane@gmail.com>
2016-03-02 22:38:30 +0000

    WIP adding redux, reorganising files

--- public/src/app.jsx
+++ public/src/app.jsx
@@ -1,6 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var AppContainer = require('./app-container.jsx');
+var AppContainer = require('./components/app-container.jsx');
 
 ReactDOM.render(
 
---
+++ src/actions.js
@@ -0,0 +1,12 @@
+'use strict';
+/*
+- get dlr arrivals
+- get bus arrivals
+- get train arrivals
+
+async:
+- is fetching (spinner)
+- has fetched (load info in, remove spinner, reset isfetching)
+- has failed to fetch (display error)
+
+*/
