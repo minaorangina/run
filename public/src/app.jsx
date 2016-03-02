5208d471434934609a3b4227296f27ddc9aa9a07
Mina <wilhelmina.jane@gmail.com>
2016-03-02 23:49:51 +0000

    Sassifies app

--- public/src/app.jsx
+++ public/src/app.jsx
@@ -2,6 +2,8 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var AppContainer = require('./components/app-container.jsx');
 
+require('./scss/main.scss');
+
 ReactDOM.render(
 
     <AppContainer />,
