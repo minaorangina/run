21a2330877c37095e7a42f9bcabe313a212b34ba
Mina <wilhelmina.jane@gmail.com>
2016-03-06 09:55:25 +0000

    adds whitespace

--- start.js
+++ start.js
@@ -2,6 +2,7 @@ var Hapi = require('hapi');
 var Path = require('path');
 var inert = require('inert');
 var routes = require('./routes.js');
+
 var server = new Hapi.Server({
 
     connections: {
@@ -18,7 +19,9 @@ server.register(inert, function () {});
 server.connection({
     port: Number(process.env.PORT || 8000)
 });
+
 server.route(routes);
+
 server.start(function () {
 
     console.log("Server running at port:", server.info.uri);
