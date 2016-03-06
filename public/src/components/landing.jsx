e277d6d46272215614a0c539b5089595cd14d20d
Mina <wilhelmina.jane@gmail.com>
2016-03-06 11:44:57 +0000

    sets up DOM testing with es6

--- public/src/components/landing.jsx
+++ public/src/components/landing.jsx
@@ -1,1 +1,19 @@
 'use strict';
+
+import React from 'react';
+
+const Landing = React.createClass({
+
+    render () {
+
+        return (
+            <div>
+                <h1>
+                    Home or Away?
+                </h1>
+            </div>
+        );
+    }
+})
+
+export default Landing
