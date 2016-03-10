1a9e2fc9689a0c6f98feac1a7f785998f7485a47
Mina <wilhelmina.jane@gmail.com>
2016-03-10 21:31:53 +0000

    separate webpack config files for separate concerns

---
+++ webpack.production.config.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var webpack = require('webpack');
+
+module.exports = {
+    entry: [
+        './public/src/app.jsx'
+    ],
+    output: {
+        path: __dirname,
+        filename: "./public/bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: "babel-loader",
+                exclude: /node_modules/,
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            },
+            {
+                test: /\.scss$/,
+                loaders: ["style-loader", "css-loader", "sass-loader"]
+            }
+        ]
+    },
+    inline    : true,
+    progress  : true,
+    colors    : true
+};
