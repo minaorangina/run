088872c4f3269d54032f50b7ea550f6c11738a57
minaorangina <wilhelmina.jane@gmail.com>
2016-12-30 12:47:48 +0000

    displays train eta

--- src/js/components/train.jsx
+++ src/js/components/train.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 
-export default function Train ({ data }) {
-
-    const destination = 'this will be the destination';
-    const arrivals = data.map((arrival, i) => {
+export default function Train ({ data, destination }) {
+    
+    const arrivals = data.length > 0 && data.map((arrival, i) => {
+        const trainDestination = arrival.destination.location[0].locationName;
         return (
             <div className="arrival-item" key={ i }>
-                { arrival }
+                { `${arrival.std} to ${trainDestination}` }
+                <div className="info">{ arrival.etd }</div>
             </div>
         );
     });
 
     return (
         <div className='card'>
-            <h3>{ `Train: ${destination}` }</h3>
+            <h3>{ `Train: going to ${destination}` }</h3>
             { arrivals }
         </div>
     );
