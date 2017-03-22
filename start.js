135627f295c315a9bb18342e861325ba70b49ff1
Mina <wilhelmina.jane@gmail.com>
2017-03-22 00:19:05 +0000

    tidy up. edits postinstall path

--- start.js
+++ start.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const http = require('http');
-const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const socket = require('socket.io');
