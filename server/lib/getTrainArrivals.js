9c86589cb3439050e072f37f3e0b90bffe349761
Mina <wilhelmina.jane@gmail.com>
2018-05-04 22:53:35 +0100

    updates

--- server/lib/getTrainArrivals.js
+++ server/lib/getTrainArrivals.js
@@ -1,79 +1,89 @@
-"use strict";
-const soap = require('soap');
-const _isEqual = require('lodash.isequal');
-const sendBackData = require('./sendBackData');
+const soap = require("soap");
+const _isEqual = require("lodash.isequal");
+const sendBackData = require("./sendBackData");
 
-const url = 'https://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx?ver=2015-05-14';
-const accessToken = '<AccessToken><TokenValue>' + process.env.TOKEN + '</TokenValue></AccessToken>';
+const url =
+  "https://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx?ver=2017-02-02";
+const accessToken = `<AccessToken><TokenValue>${
+  process.env.TOKEN
+}</TokenValue></AccessToken>`;
 
-let INTERVAL_ID = '';
+let INTERVAL_ID = "";
 let dataCache = {};
-const mode = 'train';
+const mode = "train";
 
-async function getTrainArrivals (io, direction, useCache) {
-
-    if (INTERVAL_ID) {
-        clearInterval(INTERVAL_ID);
+async function getTrainArrivals(io, direction, useCache) {
+  if (INTERVAL_ID) {
+    clearInterval(INTERVAL_ID);
+  }
+  if (Object.keys(io.nsp.connected).length > 0) {
+    await pollAPI(io, direction);
+    if (useCache && Object.keys(dataCache).length > 0) {
+      sendBackData(io, dataCache);
     }
-    if (Object.keys(io.nsp.connected).length > 0) {
-        await pollAPI(io, direction);
-        if (useCache && Object.keys(dataCache).length > 0) {
-            sendBackData(io, dataCache);
-        }
-    }
+  }
 }
 
-function pollAPI (io, direction) {
-    INTERVAL_ID = setInterval(() => {
-        getTrainArrivals(io, direction);
-    }, 10000);
-    return new Promise((resolve, reject) => {
-        soap.createClient(url, function (err, client) {
+function pollAPI(io, direction) {
+  INTERVAL_ID = setInterval(() => {
+    getTrainArrivals(io, direction);
+  }, 10000);
+  return new Promise((resolve, reject) => {
+    soap.createClient(url, function(err, client) {
+      if (err) {
+        console.error("Error creating client...");
+        io.emit(
+          `${mode}:error`,
+          new Error(`Error creating soap client ${err.message}`)
+        );
+        return reject();
+      }
 
-            if (err) {
-                console.error('Error creating client...');
-                io.emit(`${mode}:error`, new Error(`Error creating soap client ${err.message}`));
-                return reject();
-            }
+      const away = {
+        numRows: 9,
+        crs: process.env.AWAYWARDS_ORIGIN_TRAIN,
+        filterCrs: process.env.AWAYWARDS_DESTINATION_TRAIN,
+        filterType: "to",
+        timeOffset: 0,
+        timeWindow: 120
+      };
 
-            const away = {
-                numRows: 9,
-                crs: process.env.AWAYWARDS_ORIGIN_TRAIN,
-                filterCrs: process.env.AWAYWARDS_DESTINATION_TRAIN,
-                filterType: 'to',
-                timeOffset: 0,
-                timeWindow: 120
-            };
+      const home = {
+        numRows: 9,
+        crs: process.env.HOMEWARDS_ORIGIN_TRAIN,
+        filterCrs: process.env.AWAYWARDS_ORIGIN_TRAIN,
+        filterType: "to",
+        timeOffset: 0,
+        timeWindow: 120
+      };
 
-            const home = {
-                numRows: 9,
-                crs: process.env.HOMEWARDS_ORIGIN_TRAIN,
-                filterCrs: process.env.AWAYWARDS_ORIGIN_TRAIN,
-                filterType: 'to',
-                timeOffset: 0,
-                timeWindow: 120
-            };
-
-            const args = direction === 'home' ? home : away;
-            client.addSoapHeader(accessToken);
-            client.GetDepBoardWithDetails(args, function (err, result) {
-                if (err) {
-                    console.error('Error getting departures...');
-                    console.error(`${mode}:error`, err);
-                    io.emit(`${mode}:error`, err);
-                    return reject();
-                }
-                const stationBoard = result.GetStationBoardResult;
-                const data = stationBoard.trainServices ? stationBoard.trainServices.service.slice(0, 6) : [];
-                const currentData = { data, mode: 'train', direction, destination: stationBoard && stationBoard.filterLocationName };
-                if (!_isEqual(currentData, dataCache)) {
-                    dataCache = { ...currentData };
-                    sendBackData(io, dataCache);
-                }
-                resolve();
-            });
-        });
+      const args = direction === "home" ? home : away;
+      client.addSoapHeader(accessToken);
+      client.GetDepartureBoard(args, function(err, result) {
+        if (err) {
+          console.error("Error getting departures...");
+          console.error(`${mode}:error`, err);
+          io.emit(`${mode}:error`, err);
+          return reject();
+        }
+        const stationBoard = result.GetStationBoardResult;
+        const data = stationBoard.trainServices
+          ? stationBoard.trainServices.service.slice(0, 6)
+          : [];
+        const currentData = {
+          data,
+          mode: "train",
+          direction,
+          destination: stationBoard && stationBoard.filterLocationName
+        };
+        if (!_isEqual(currentData, dataCache)) {
+          dataCache = { ...currentData };
+          sendBackData(io, dataCache);
+        }
+        resolve();
+      });
     });
+  });
 }
 
 module.exports = getTrainArrivals;
--- server/lib/sendBackData.js
+++ server/lib/sendBackData.js
@@ -1,43 +1,47 @@
-
-function sendBackData (io, { data = [], mode, direction, destination }) {
-    const origin = direction === 'away' ? process.env.AWAYWARDS_ORIGIN_TRAIN_NAME : process.env.AWAYWARDS_DESTINATION_TRAIN_NAME;
-    parseTrainData(data)
+function sendBackData(io, { data = [], mode, direction, destination }) {
+  const origin =
+    direction === "away"
+      ? process.env.AWAYWARDS_ORIGIN_TRAIN_NAME
+      : process.env.AWAYWARDS_DESTINATION_TRAIN_NAME;
+  parseTrainData(data)
     .then((parsed = []) => {
-        io.emit(`${mode}:arrivals`, {
-            android_data: parsed,
-            data,
-            direction,
-            origin,
-            destination,
-            last_updated: new Date().toISOString()
-        });
+      io.emit(`${mode}:arrivals`, {
+        android_data: parsed,
+        data,
+        direction,
+        origin,
+        destination,
+        last_updated: new Date().toISOString()
+      });
     })
     .catch(console.error);
 }
 
-function parseTrainData (trainData, mode = 'train') {
-    return new Promise((resolve) => {
-        if (trainData.length === 0) {
-            return resolve();
-        }
-        const mapped = trainData.map(arrival => {
-            const terminus = arrival.destination.location[0].locationName;
-            return {
-                std: arrival.std,
-                etd: arrival.etd,
-                terminus: normaliseStationName(terminus, mode)
-            };
-        });
-        resolve(mapped);
+function parseTrainData(trainData, mode = "train") {
+  return new Promise(resolve => {
+    if (trainData.length === 0) {
+      return resolve();
+    }
+    const mapped = trainData.map(arrival => {
+      const terminus = arrival.destination.location[0].locationName;
+      return {
+        std: arrival.std,
+        etd: arrival.etd,
+        terminus: normaliseStationName(terminus, mode),
+        cancelReason: arrival.cancelReason,
+        delayReason: arrival.delayReason
+      };
     });
+    resolve(mapped);
+  });
 }
-function normaliseStationName (stationName, mode) {
-    if (mode === 'dlr' && stationName.length > 0) {
-        return stationName.replace(' DLR Station', '');
-    }
-    if (mode === 'train' && stationName !== 'London Bridge') {
-        return stationName.replace('London ', '');
-    }
+function normaliseStationName(stationName, mode) {
+  if (mode === "dlr" && stationName.length > 0) {
+    return stationName.replace(" DLR Station", "");
+  }
+  if (mode === "train" && stationName !== "London Bridge") {
+    return stationName.replace("London ", "");
+  }
 }
 
 module.exports = sendBackData;
